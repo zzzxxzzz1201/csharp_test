81544e938cd6786f5a3f7804f71b916817ac32b7
zzzxxzzz1201 <58492575+zzzxxzzz1201@users.noreply.github.com>
2020-08-05 02:05:43 +0800

    new

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(394, 116);
             this.button1.TabIndex = 0;
-            this.button1.Text = "test_dev";
+            this.button1.Text = "test_dev111";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
